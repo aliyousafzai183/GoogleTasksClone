24ffd6ab8eb8f4283a8d5e682ee08f7ac6ebdc44
aliyousafzai183 <warisallah923@gmail.com>
2023-04-05 15:58:35 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.mainContainer}>
+      <Text>ALLAH IS GREAT ! I LOVE ALLAH PAK</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    padding:'3%'
+  }
+});
+
+export default App;
