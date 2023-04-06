b9206b2d8a943b2af17e4e700cc4da4c6ad8a3dd
aliyousafzai183 <warisallah923@gmail.com>
2023-04-06 23:11:54 +0500

    Starting everything from scratch because issue

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
-
 import * as React from 'react';
 import {
-  StyleSheet,
-  Text,
   View,
+  Text
 } from 'react-native';
-
-// screens
-import NoTasksScreen from './src/views/NoTasksYetScreen';
-import NoStarredTasksScreen from './src/views/NoStarredTaskScreen';
 
 function App() {
   return (
-    <View style={styles.mainContainer}>
-      {/* <NoTasksScreen /> */}
-      <NoStarredTasksScreen />
+    <View>
+      <Text>Hello</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    padding:'3%'
-  }
-});
 
 export default App;
